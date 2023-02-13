f0c3e8b17a3af9225b69d287203a89a7985971b1
massdosage <massdosage@gmail.com>
2023-02-13 23:11:36 +0000

    fix tests

--- lb_content_resolver/test/formats/test_tag_utils.py
+++ lb_content_resolver/test/formats/test_tag_utils.py
@@ -1,19 +1,20 @@
-from lb_content_resolver.formats.tag_utils import TagUtils
+from lb_content_resolver.formats.tag_utils import extract_track_number
 
 
 class TestTagUtils:
 
     def test_int(self):
-        assert TagUtils.extract_track_number(100) == 100
-        assert TagUtils.extract_track_number(-100) == -100
+        assert extract_track_number(100) == 100
+        assert extract_track_number(-100) == -100
 
     def test_string(self):
-        assert TagUtils.extract_track_number("100") == 100
+        assert extract_track_number("100") == 100
 
     def test_string_with_slash(self):
-        assert TagUtils.extract_track_number("9/12") == 9
+        assert extract_track_number("9/12") == 9
 
     def test_invalid_values(self):
-        assert TagUtils.extract_track_number("") == 0
-        assert TagUtils.extract_track_number("NotANumber") == 0
-        assert TagUtils.extract_track_number("3.0") == 0
+        assert extract_track_number(None) == None
+        assert extract_track_number("") == None
+        assert extract_track_number("NotANumber") == None
+        assert extract_track_number("3.0") == None
