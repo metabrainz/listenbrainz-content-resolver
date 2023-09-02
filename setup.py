04cc714aae4ef6d77325e863b607ddb2f72ffa05
Robert Kaye <rob@musicbrainz.org>
2023-09-03 00:21:29 +0200

    Improve setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ListenBrainz Content Resolver',
     version='1.0.0',
@@ -7,5 +10,16 @@ setup(
     author='Robert Kaye',
     author_email='rob@metabrainz.org',
     description='A library and command line tool for taking MBID based JSPF playlists and resolving them to a local collection of tracks.',
-    packages=find_packages()    
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[ "Click==8.1.3", "mutagen==1.46.0", "Unidecode==1.3.6", "scikit-learn==1.2.1", "nmslib==2.1.1", "regex==2023.6.3"],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
