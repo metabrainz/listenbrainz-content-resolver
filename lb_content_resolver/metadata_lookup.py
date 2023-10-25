f5819b430050151e9c1caef21707351aa5114e3d
Robert Kaye <rob@musicbrainz.org>
2023-10-25 13:04:08 +0200

    Finished implementing the popularity and tag lookup

--- lb_content_resolver/metadata_lookup.py
+++ lb_content_resolver/metadata_lookup.py
@@ -20,6 +20,7 @@ class MetadataLookup:
 
     def lookup(self):
         """
+        Iterate over all recordings in the database and call lookup_chunk for chunks of recordings.
         """
 
         self.db.open_db()
@@ -35,7 +36,19 @@ class MetadataLookup:
             args.append({ "[recording_mbid]": mbid })
             mbid_to_id_index[mbid] = row
             if len(args) == 1000:
-                break
+                self.lookup_chunk(args, mbid_to_id_index)
+                args = []
+                mbid_to_id_index = {}
+
+        if len(args) > 0:
+            self.lookup_chunk(args, mbid_to_id_index)
+
+
+    def look_chunk(self, args, mbid_to_id_index):
+        """
+            This function carries out the actual lookup of the metadata and inserting the
+            popularity and tags into the DB for the given chunk of recordings.
+        """
 
         r = requests.post("https://labs.api.listenbrainz.org/bulk-tag-lookup/json", json=args)
         if r.status_code != 200:
@@ -43,11 +56,9 @@ class MetadataLookup:
             return
 
         recording_pop = {}
-        recording_tags = {}  ## still needed? TODO
+        recording_tags = {}
         tags = set()
         for row in r.json():
-            print("%s, %s, %s" % (row["recording_mbid"], row["tag"], row["source"]))
-
             mbid = str(row["recording_mbid"])
             recording_pop[mbid] = row["percent"]
             if mbid not in recording_tags:
@@ -57,15 +68,15 @@ class MetadataLookup:
             tags.add(row["tag"])
 
         tags = list(tags)
-
-        print(f"{len(args)} db rows, {len(r.json())} api rows")
-
         with db.atomic():
+
+            # This DB code is pretty messy -- things I take for granted with Postgres are not
+            # available in SQLite or the PeeWee ORM. But, this might be ok, since we're not 
+            # updating millions of rows constantly.
 
             # First update recording_metadata table
             mbids = recording_pop.keys()
             for mbid in list(set(mbids)):
-                print(f"update {mbid}")
                 mbid = str(mbid)
                 row = mbid_to_id_index[mbid]
                 if row[3] is None:
@@ -89,11 +100,20 @@ class MetadataLookup:
                                   )""", (mbid,))
 
             # Finally, insert new recording tags
+
+            # Sqlite doesn't seem to support RETURNING with OR IGNORE, so we have to do this in two steps.
+            tag_ids = {}
             for tag in tags:
                 db.execute_sql("""INSERT OR IGNORE INTO tag (name) VALUES (?)""", (tag,))
 
+            # And I can't see how to get sqlite to take a list as an argument for this.
+            tag_str = ",".join([ "'%s'" % t for t in tags])
+            cursor = db.execute_sql("""SELECT id, name FROM tag WHERE name IN (%s)""" % tag_str)
+            for row in cursor.fetchall():
+                tag_ids[row[1]] = row[0]
+
             for row in r.json():
-                print("%s, %s, %s" % (row["recording_mbid"], row["tag"], row["source"]))
                 row_id = mbid_to_id_index[row["recording_mbid"]]
-                db.execute_sql("""INSERT INTO recording_tag (recording_id, tag, entity)
-                                       VALUES (?, ?, ?)""", (row_id, row["tag"], row["source"]))
+                now = datetime.datetime.now()
+                db.execute_sql("""INSERT INTO recording_tag (recording_id, tag_id, entity, last_updated)
+                                       VALUES (?, ?, ?, ?)""", (row_id[0], tag_ids[row["tag"]], row["source"], now))
