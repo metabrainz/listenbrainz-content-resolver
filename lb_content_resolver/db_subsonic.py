c74177bf5933be07db84bc7e630ec45f84c205b0
Robert Kaye <rob@musicbrainz.org>
2023-11-29 23:07:44 +0100

    Starting to add subsonic support and build sync feature

---
+++ lb_content_resolver/db_subsonic.py
@@ -0,0 +1,62 @@
+import os
+
+import libsonic
+
+from lb_content_resolver.database import Database
+import config
+
+
+class SubsonicDatabase(Database):
+    ''' 
+    Keep a database with metadata for a collection of files via a subsonic API
+    '''
+
+    MAX_ALBUMS_PER_CALL = 10  # 500
+
+    def __init__(self, index_dir):
+        super(SubsonicDatabase, self).__init__(index_dir)
+
+    def sync(self):
+        """
+            Scan the subsonic client specified in config.py
+        """
+
+        # Keep some stats
+        self.total = 0
+        self.added = 0
+        self.removed = 0
+        self.updated = 0
+
+        self.open_db()
+        self.run_sync()
+        self.close_db()
+
+        print("Checked %s tracks:" % self.total)
+        print("  %5d tracks added" % self.added)
+        print("  %5d tracks updated" % self.updated)
+        print("  %5d tracks removed" % self.removed)
+
+    def run_sync(self):
+
+        print("Connect to subsonic..")
+        conn = libsonic.Connection(config.SUBSONIC_HOST, config.SUBSONIC_USER, config.SUBSONIC_PASSWORD, config.SUBSONIC_PORT)
+
+        print("Fetch recordings")
+        recordings = []
+        album_count = 0
+        while True: 
+            albums_this_batch = 0;
+            albums = conn.getAlbumList(ltype="alphabeticalByArtist", size=self.MAX_ALBUMS_PER_CALL, offset=album_count)
+
+            for album in albums["albumList"]["album"]:
+                album_info = conn.getAlbum(id=album["id"])
+                for song in album_info["album"]["song"]:
+                    recordings.append((song["id"], song["path"]))
+                album_count += 1
+                albums_this_batch += 1
+
+            print("fetched %d releases" % albums_this_batch)
+            if albums_this_batch < self.MAX_ALBUMS_PER_CALL:
+                break
+
+        print("loaded %s recordings" % len(recordings))
--- resolve.py
+++ resolve.py
@@ -4,6 +4,7 @@ import os
 from lb_content_resolver.content_resolver import ContentResolver
 from lb_content_resolver.database import Database
 from lb_content_resolver.db_filesystem import FilesystemDatabase
+from lb_content_resolver.db_subsonic import SubsonicDatabase
 from lb_content_resolver.metadata_lookup import MetadataLookup
 import click
 
@@ -23,14 +24,14 @@ def create(index_dir):
 @click.command()
 @click.argument('index_dir')
 @click.argument('music_dir')
-def scan_dir(index_dir, music_dir):
+def scan(index_dir, music_dir):
     db = FilesystemDatabase(index_dir)
     db.scan(music_dir)
 
 
 @click.command()
 @click.argument('index_dir')
-def cleanup_dir(index_dir):
+def cleanup(index_dir):
     db = FilesystemDatabase(index_dir)
     db.database_cleanup()
 
@@ -45,6 +46,13 @@ def lookup(index_dir):
 
 @click.command()
 @click.argument('index_dir')
+def subsonic(index_dir):
+    db = SubsonicDatabase(index_dir)
+    db.sync()
+
+
+@click.command()
+@click.argument('index_dir')
 @click.argument('jspf_playlist')
 @click.argument('m3u_playlist')
 @click.option('-t', '--threshold', default=.80)
@@ -55,10 +63,11 @@ def playlist(index_dir, jspf_playlist, m3u_playlist, threshold):
 
 
 cli.add_command(create)
-cli.add_command(scan_dir)
+cli.add_command(scan)
 cli.add_command(playlist)
-cli.add_command(cleanup_dir)
+cli.add_command(cleanup)
 cli.add_command(lookup)
+cli.add_command(subsonic)
 
 
 def usage(command):
