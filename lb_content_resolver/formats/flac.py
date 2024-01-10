e4d5817a4e642751b5021e22ba78450035ba113f
Laurent Monin <github@norz.org>
2024-01-10 17:07:00 +0100

    Do not raise an exception when disc number is missing, defaults to 1

--- lb_content_resolver/formats/flac.py
+++ lb_content_resolver/formats/flac.py
@@ -19,7 +19,7 @@ def read(file):
     mdata["release_name"] = get_tag_value(tags, "album")
     mdata["recording_name"] = get_tag_value(tags, "title")
     mdata["track_num"] = extract_track_number(get_tag_value(tags, "tracknumber"))
-    mdata["disc_num"] = int(get_tag_value(tags, "discnumber"))
+    mdata["disc_num"] = int(get_tag_value(tags, "discnumber") or 1)
     mdata["artist_mbid"] = get_tag_value(tags, "musicbrainz_artistid")
     mdata["recording_mbid"] = get_tag_value(tags, "musicbrainz_trackid")
     mdata["release_mbid"] = get_tag_value(tags, "musicbrainz_albumid")
